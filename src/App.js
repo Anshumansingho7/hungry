9f29b17e7f84901df6b00ed700cd0d147ce5624f
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-08-29 20:49:49 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './screens/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './screens/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
