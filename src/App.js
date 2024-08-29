90e81d7d32749e4d69f3248c3b373a1480137ed7
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-08-29 20:36:25 +0530

    use react-router-dom so the page not refresh again and again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Home from './screens/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
-        <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
